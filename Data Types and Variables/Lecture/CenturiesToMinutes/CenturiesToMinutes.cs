05bada20a83348a92576ed509aca55585b59834b
Buddyland <justbuddy09@gmail.com>
2017-09-24 16:10:37 +0200

    Updated name of Lecture folder (removed 's')

---
+++ Data Types and Variables/Lecture/CenturiesToMinutes/CenturiesToMinutes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CenturiesToMinutes
+{
+    public class CenturiesToMinutes
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Centuries = ");
+            int centuries = int.Parse(Console.ReadLine());
+
+            int years = 100 * centuries;
+            int days = (int)(years * 365.2422); // it is not 365, because that's a tropic year
+            int hours = 24 * days;
+            int minutes = 60 * hours;
+
+            // it is so printed from the lecture
+            // Console.WriteLine($"Centuries = {centuries}\nYears = {years}\nDays = {days}\nHours = {hours}\nMinutes = {minutes}");
+
+            // so is more readable
+            Console.WriteLine($"Centuries = {centuries}\n" +
+                              $"Years = {years}\n" +
+                              $"Days = {days}\n" +
+                              $"Hours = {hours}\n" +
+                              $"Minutes = {minutes}");
+        }
+    }
+}
---
+++ Data Types and Variables/Lecture/RefactorVolumeOfPyramid/RefactorVolumeOfPyramid.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorVolumeOfPyramid
+{
+    public class RefactorVolumeOfPyramid
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Length: ");
+            double lenght = double.Parse(Console.ReadLine());
+            
+            Console.Write("Width: ");
+            double width = double.Parse(Console.ReadLine());
+
+            Console.Write("Height: ");
+            double height = double.Parse(Console.ReadLine());
+
+            var result = (lenght * width * height) / 3;
+            Console.WriteLine("Pyramid Volume: {0:F2}", result);
+        }
+    }
+}
